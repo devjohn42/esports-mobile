0702b15a2b651ff5d3cea8e1c8b40626c090475a
devjohn42 <devjohncb@gmail.com>
2026-04-01 19:46:52 -0300

    :lipstick: feat: creates status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
+import { StatusBar } from 'react-native'
 import { Background } from './src/_components/background'
 
 export default function App() {
-	return <Background></Background>
+	return (
+		<Background>
+			<StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+		</Background>
+	)
 }
