fc3bfaaa90f54df6b0b00ed2c918ed914b651c88
devjohn42 <devjohncb@gmail.com>
2026-04-03 18:08:31 -0300

    :lipstick: feat: creates the heading component

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,14 @@
-import { View } from 'react-native'
+import { Image, View } from 'react-native'
+import { Heading } from '../../_components/heading'
+import logo from '../../assets/logo-nlw-esports.png'
 import { styles } from './styles'
 
 export function Home() {
-	return <View style={styles.container}></View>
+	return (
+		<View style={styles.container}>
+			<Image source={logo} style={styles.logo} />
+
+			<Heading title="Find your duo!" subtitle="Find your game freedom" />
+		</View>
+	)
 }
