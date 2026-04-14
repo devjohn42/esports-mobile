1bd33ae33b33d10dc315bc55a4fb6ae35364b020
devjohn42 <devjohncb@gmail.com>
2026-04-14 20:16:40 -0300

    :sparkles: feat: creates connection with server api

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react'
 import { FlatList, Image, View } from 'react-native'
-import { GameCard } from '../../_components/gamecard/gameCard'
+import { GameCard, type GameCardProps } from '../../_components/gamecard/gameCard'
 import { Heading } from '../../_components/heading'
 import logo from '../../assets/logo-nlw-esports.png'
-import { GAMES } from '../../utils/games'
 import { styles } from './styles'
 
 export function Home() {
+	const [games, setGames] = useState<GameCardProps[]>([])
+
+	useEffect(() => {
+		fetch('http://192.168.0.3:3333/games')
+			.then((response) => response.json())
+			.then((data) => setGames(data))
+	}, [])
+
 	return (
 		<View style={styles.container}>
 			<Image source={logo} style={styles.logo} />
@@ -13,7 +21,7 @@ export function Home() {
 			<Heading title="Find your duo!" subtitle="Find your game freedom" />
 			<FlatList
 				contentContainerStyle={styles.contentList}
-				data={GAMES}
+				data={games}
 				keyExtractor={(item) => item.id}
 				renderItem={({ item }) => <GameCard data={item} />}
 				horizontal
